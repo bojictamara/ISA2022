07a4f2beeb9a258cf823cb1f8faffd8982e3f0ea
bojictamara <bojic.ra79.2018@uns.ac.rs>
2022-12-22 00:55:05 +0100

    kreirani projekti

---
+++ blood-transfusion/src/main/java/com/isa/bloodtransfusion/BloodTransfusionApplication.java
@@ -0,0 +1,13 @@
+package com.isa.bloodtransfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodTransfusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodTransfusionApplication.class, args);
+	}
+
+}
---
+++ blood-transfusion/src/test/java/com/isa/bloodtransfusion/BloodTransfusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.bloodtransfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodTransfusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
